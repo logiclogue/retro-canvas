e44ee31fae2996e9f21e9369715510508b667272
logiclogue <jordanpaullord@hotmail.co.uk>
2016-07-22 16:46:54 +0100

    Adds base methods to RetroCanvas class

--- js/RetroCanvas.js
+++ js/RetroCanvas.js
@@ -1,8 +1,20 @@
-function RetroCanvas() {
-    
+function RetroCanvas(canvas) {
+    this.canvas;
+    this.ctx;
+
+    if (!canvasId) {
+        return;
+    }
+
+    this.addCanvas(canvas);
 }
 
 (function (static_, proto_) {
+
+    proto_.addCanvas = function (canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+    };
 
 }(RetroCanvas, RetroCanvas.prototype));
 
